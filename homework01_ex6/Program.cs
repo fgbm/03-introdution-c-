119d4a44334ed7f4124e73ebd4b1e69f0a7ec37d
ChmelyukVS <i@neimp.ru>
2022-10-01 18:11:43 +0300

    Добавлен код Д/З №1, задание 6

---
+++ homework01_ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+*/
+
+int ReadInt()
+{
+    Console.Write("Введите число: ");
+    string? line = Console.ReadLine();
+    int result;
+    while (!int.TryParse(line, out result))
+    {
+        Console.Write("Это не целое число, введите число: ");
+        line = Console.ReadLine();
+    }
+    return result;
+}
+
+int number = ReadInt();
+if (number % 2 == 0) Console.WriteLine("Число чётное");
+else Console.WriteLine("Число нечётное");
