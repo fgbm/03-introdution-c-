73bb0317e2e237204af078b9707d6832ffa4164b
ChmelyukVS <i@neimp.ru>
2022-10-01 17:50:18 +0300

    Решение д/з №1, задача 2

--- homework01_ex2/Program.cs
+++ homework01_ex2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: напишите программу которая на вход принимает 2 числа и 
+выдаёт, какое число большее, а какое меньшее
+*/
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int ReadInt()
+        {
+            Console.Write("Введите число: ");
+            string? line = Console.ReadLine();
+            int result;
+            while (!int.TryParse(line, out result))
+            {
+                Console.Write("Это не целое число, введите число: ");
+                line = Console.ReadLine();
+            }
+            return result;
+        }
+
+        int? number1;
+        int? number2;
+
+        number1 = ReadInt();
+        number2 = ReadInt();
+
+        if (number1 > number2) Console.WriteLine($"Наибольшее число: {number1}");
+        if (number2 > number1) Console.WriteLine($"Наибольшее число: {number2}");
+        if (number1 == number2) Console.WriteLine("Числа равны");
+    }
+}
