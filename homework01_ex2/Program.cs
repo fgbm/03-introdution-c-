d75023d837815bdf1c3aeaca4a1cc5a54c6288e5
ChmelyukVS <i@neimp.ru>
2022-10-01 18:22:50 +0300

    Оптимизация кода

--- homework01_ex2/Program.cs
+++ homework01_ex2/Program.cs
@@ -2,31 +2,23 @@
 Задача 2: напишите программу которая на вход принимает 2 числа и 
 выдаёт, какое число большее, а какое меньшее
 */
-partial class Program
+
+int ReadInt()
 {
-    private static void Main(string[] args)
+    Console.Write("Введите число: ");
+    string? line = Console.ReadLine();
+    int result;
+    while (!int.TryParse(line, out result))
     {
-        int ReadInt()
-        {
-            Console.Write("Введите число: ");
-            string? line = Console.ReadLine();
-            int result;
-            while (!int.TryParse(line, out result))
-            {
-                Console.Write("Это не целое число, введите число: ");
-                line = Console.ReadLine();
-            }
-            return result;
-        }
-
-        int? number1;
-        int? number2;
-
-        number1 = ReadInt();
-        number2 = ReadInt();
-
-        if (number1 > number2) Console.WriteLine($"Наибольшее число: {number1}");
-        if (number2 > number1) Console.WriteLine($"Наибольшее число: {number2}");
-        if (number1 == number2) Console.WriteLine("Числа равны");
+        Console.Write("Это не целое число, введите число: ");
+        line = Console.ReadLine();
     }
+    return result;
 }
+
+int number1 = ReadInt();
+int number2 = ReadInt();
+
+if (number1 > number2) Console.WriteLine($"Наибольшее число: {number1}");
+if (number2 > number1) Console.WriteLine($"Наибольшее число: {number2}");
+if (number1 == number2) Console.WriteLine("Числа равны");
