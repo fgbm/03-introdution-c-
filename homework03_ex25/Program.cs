09e2537d555e7e104ad15fd30f2736f265db2f16
ChmelyukVS <i@neimp.ru>
2022-10-15 17:35:10 +0300

    Добавлено решение задачи №25

---
+++ homework03_ex25/Program.cs
@@ -0,0 +1,16 @@
+﻿double Pow(double x, int super)
+{
+    if (super > 0)
+    {
+        double product = 1;
+        for (int i = 0; i < super; i++)
+        {
+            product *= x;
+        }
+        return product;
+    }
+    else return -1;
+}
+
+Console.WriteLine($"3,5 => {Pow(3, 5)}");
+Console.WriteLine($"2,4 => {Pow(2, 4)}");
