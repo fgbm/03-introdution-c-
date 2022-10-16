a602b9e7c5484cbfec9b3c51bc6dbd3dd0015ea4
ChmelyukVS <i@neimp.ru>
2022-10-16 12:01:27 +0300

    Добавлено решение задачи №27

---
+++ homework03_ex27/Program.cs
@@ -0,0 +1,16 @@
+﻿int DigitSum(int number)
+{
+    int product = 0;
+    while (number / 10 != 0)
+    {
+        product += number % 10;
+        number /= 10;
+    }
+    return product + number;
+}
+
+int[] examples = { 452, 82, 9012 };
+foreach (int i in examples)
+{
+    Console.WriteLine($"{i} => {DigitSum(452)}");
+}
