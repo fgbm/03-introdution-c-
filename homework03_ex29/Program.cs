57f99457fd4727255feb96eafb802fee1c059138
ChmelyukVS <i@neimp.ru>
2022-10-16 12:16:31 +0300

    Добавлено решение задачи №29

---
+++ homework03_ex29/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+*/
+
+void WriteArray(int[] array)
+{
+    if (array.Length != 8)
+    {
+        Console.WriteLine("Массив не из 8ми элементов");
+        return;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i + 1 != array.Length) Console.Write($"{array[i]}, ");
+        else Console.WriteLine(array[i]);
+    }
+}
+
+WriteArray(new int[] { 1, 2, 5, 7, 19, 6, 1, 33 });
